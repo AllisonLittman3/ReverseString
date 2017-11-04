1e63810bc941a54342b72eda812a9744f8898632
Allison Littman <32345409+AllisonLittman3@users.noreply.github.com>
2017-11-04 17:07:30 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Starts with string "LaunchCode"
+            string myString = "LaunchCode";
+
+            //Converts string into a character array
+            char[] myArray = myString.ToCharArray();
+            
+            //Reverses the character array
+            Array.Reverse(myArray);
+            
+            //Converts character array back into a string, replaces old string value
+            myString = new string(myArray);
+
+            //Writes the reversed string
+            Console.WriteLine(myString);
+            Console.ReadLine();
+
+        }
+    }
+}
